5fa91f7b549ff93765b4d87c9e4e8dd01ce2dc1e
Renata <renatasna@msn.com>
2022-02-21 22:13:11 -0300

    estudos JavaScript

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,27 @@
+let js = 'amazing';
+
+console.log(40+8+23-10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilde"; /* declarei a variável no formato Camel case*/
+
+console.log("firstName");
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilde = "JM";
+let $function = 27;
+let name = "Jonas"; /*nunca chamar minha variável apenas de name, pois name já uma palavra reservada*/
+
+/*não utilizar letras maiúsculas na variável*/
+
+let myFirstJob = 'programmer'; /*forma correta de declarar um nomel para variável*/
+let myCurrentJob = 'teacher';
+
+let job1 = 'programmer'; /*forma incorreta de declarar nome para varável, o código nao fica limpo e lógico*/
+let job2 = 'teacher';
