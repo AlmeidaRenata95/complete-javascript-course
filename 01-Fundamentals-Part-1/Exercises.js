63f62493271ed49c378e375ed8dece3e716b8c33
Renata <renatasna@msn.com>
2022-02-24 20:37:31 -0300

    executando o exercício 4 do - challenge 1 do curso

--- 01-Fundamentals-Part-1/Exercises.js
+++ 01-Fundamentals-Part-1/Exercises.js
@@ -24,13 +24,48 @@ const language='portuguese';
 let isIsland="false";
 let population='212,6';
 let country='Brasil';
-const language="english";
+//const language="english";
+
 
 console.log(isIsland);
 console.log(population);
 console.log(country);
 console.log(language);
 */
+
+
+/*
+const language='portuguese';
+let isIsland="false";
+let population='212,6';
+let country='Brasil';
+// const language="english"; undefined pois alterei miunha variavel 
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+*/
+
+//LECTURE: Basic Operators
+let country='Brasil';
+let continent ='América do Sul';
+const language='portuguese';
+let isIsland="false";
+let population= 212.6;
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+const description1 = 
+country + ' está na ' + continent + ', e seus ' + population + ' milhões as pessoas falam ' + language; 
+console.log(description1);
+
+
+
+
+
 
 
 
